b9ba6e9b3a36b4b563b600a193b703a0841b6d6a
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-15 09:34:31 +0530

    Added:Validate user entry for last name first name email mobile password

--- src/com/blz/userregistration/UserRegistration.java
+++ src/com/blz/userregistration/UserRegistration.java
@@ -1,25 +1,86 @@
 package com.blz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * User needs to clear all email samples provided separately.
- */
-
 public class UserRegistration {
-    public static void main(String[] args) {
-        String[] validEmailSample = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
-        String[] invalidEmailSample = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+	// Scanner class for user input
+	Scanner sc = new Scanner(System.in);
 
-        System.out.println("Valid email :");
-        emailValidate(validEmailSample);
-        System.out.println("\nInvalid emails :");
-        emailValidate(invalidEmailSample);
-    }
+	// method to check username Valid or Invalid
+	public boolean validateUsername(String userName) {
+		// regex pattern for username
+		String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher matcher = p.matcher(userName);
+		boolean result = matcher.matches();
+		if(result)
+			return true;
+		else
+			return false;
+	}
 
-    public static void emailValidate(String[] emails) {
-        for(int i = 0; i< emails.length; i++ ){
-            System.out.print(Pattern.matches("^[\\w+-]+(?:\\.[\\w+-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", emails[i]) + " ");
-        }
-    }
+	// method to check LastName Valid or Invalid
+	public boolean validateLastname(String lastName) {
+		// regex pattern for Lastname
+		String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher matcher = p.matcher(lastName);
+		boolean result = matcher.matches();
+		if(result)
+			return true;
+		else
+			return false;
+	}
+
+	// method to check Email Valid or Invalid
+	public boolean validateEmail(String email) {
+		/*
+		 * regex pattern for email 1)must contain character before @ 2)must contain @
+		 * symbol after char 3)must contain char after @ 4)must contain "." symbol
+		 * before com or in
+		 */
+		String regex = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?";
+		Pattern p = Pattern.compile(regex);
+		Matcher matcher = p.matcher(email);
+		boolean result = matcher.matches();
+
+		if(result)
+			return true;
+		else
+			return false;
+	}
+
+	// method to check Phoneno Valid or Invalid
+	public boolean validateMobileNo(String mobileno) {
+		// regex pattern for email
+		String regex = "^[1-9]{2}[0-9]{10}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher matcher = p.matcher(mobileno);
+		boolean result = matcher.matches();
+		if(result)
+			return true;
+		else
+			return false;
+	}
+
+	// method to check password Valid or Invalid
+	public boolean validatePassword(String password) {
+		/*
+		 * regex pattern for password: 1)must contain atleast 8 characters 2)must
+		 * contain one UpperCase 3)should have 1 numericno 4)has contain Exactily one
+		 * specialSymbol.
+		 */
+		String regex = "^[A-Z]{1}+[a-zA-z1-9]{5,}[@$^]{1}[1-9]{1}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher matcher = p.matcher(password);
+		boolean result = matcher.matches();
+		if(result)
+			return true;
+		else
+			return false;
+
+
+	}
 }
