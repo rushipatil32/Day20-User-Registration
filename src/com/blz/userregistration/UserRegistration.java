da6d0594c9d7e162577d4167cbf919f897a875c9
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-15 09:06:55 +0530

    Added:Password having one special character

--- src/com/blz/userregistration/UserRegistration.java
+++ src/com/blz/userregistration/UserRegistration.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 /**
  * User need to follow pre-defined Password Rules.
- * Rule-3 : Should have at least 1 numeric number in the password.
+ * Rule-4 : Has exactly 1 Special Character.
  */
 
 public class UserRegistration {
@@ -13,6 +13,6 @@ public class UserRegistration {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter password : ");
         String password = scanner.next();
-        System.out.println(Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$", password));
+        System.out.println(Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=[^@#!$%^&+=]*[@#!$%^&+=][^@#!$%^&+=]*$)(?=.*[0-9]).{8,}$", password));
     }
 }
