812e4e8d0ba2448cd141aa4fc38b367a45f70632
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-15 08:46:57 +0530

    Added:Enter valid last name

--- src/com/blz/userregistration/UserRegistration.java
+++ src/com/blz/userregistration/UserRegistration.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
- * User Need to Enter Valid First Name
+ * User Need to Enter Valid Lasst Name
  */
 public class UserRegistration {
     public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first Name : ");
-        String firstName = scanner.next();
-        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+        System.out.print("Enter last Name : ");
+        String lastName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", lastName));
     }
 }
