e315b5dfa78b2ebb6e83f68d7574c204d072d437
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-15 08:41:21 +0530

    Added:ValidateFirstName

---
+++ src/com/blz/userregistration/UserRegistration.java
@@ -0,0 +1,16 @@
+package com.blz.userregistration;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+/**
+ * User Need to Enter Valid First Name
+ */
+public class UserRegistration {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first Name : ");
+        String firstName = scanner.next();
+        System.out.println(Pattern.matches("[A-Z][a-z]{2,}", firstName));
+    }
+}
