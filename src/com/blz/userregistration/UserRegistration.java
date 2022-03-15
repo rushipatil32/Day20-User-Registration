d702273e39afa6243e6daf76690215b786b9a885
Rushikesh Patil <rushipatil6632@gmail.com>
2022-03-15 09:10:43 +0530

    Added:Should clear all email samples

--- src/com/blz/userregistration/UserRegistration.java
+++ src/com/blz/userregistration/UserRegistration.java
@@ -1,18 +1,25 @@
 package com.blz.userregistration;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 /**
- * User need to follow pre-defined Password Rules.
- * Rule-4 : Has exactly 1 Special Character.
+ * User needs to clear all email samples provided separately.
  */
 
 public class UserRegistration {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter password : ");
-        String password = scanner.next();
-        System.out.println(Pattern.matches("^(?=.*[A-Z])(?=.*[a-z])(?=[^@#!$%^&+=]*[@#!$%^&+=][^@#!$%^&+=]*$)(?=.*[0-9]).{8,}$", password));
+        String[] validEmailSample = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"};
+        String[] invalidEmailSample = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+
+        System.out.println("Valid email :");
+        emailValidate(validEmailSample);
+        System.out.println("\nInvalid emails :");
+        emailValidate(invalidEmailSample);
+    }
+
+    public static void emailValidate(String[] emails) {
+        for(int i = 0; i< emails.length; i++ ){
+            System.out.print(Pattern.matches("^[\\w+-]+(?:\\.[\\w+-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$", emails[i]) + " ");
+        }
     }
 }
